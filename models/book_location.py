9e431d13f00d63acec06d0cf1c07f616e002081c
facundo-novobi <106655320+facundo-novobi@users.noreply.github.com>
2022-07-18 17:44:59 -0300

    Modified the counting of total books

--- models/book_location.py
+++ models/book_location.py
@@ -11,14 +11,10 @@ class BookLocation(models.Model):
 
     total_available_books = fields.Integer(compute='_compute_total_available_books')
 
+
     @api.depends('books')
     def _compute_total_available_books(self):
         tot_books = 0
         for record in self:
-            current_location = record.name
-            for record_inside_books in self.books:
-                if record_inside_books.location.name == current_location:
-                    tot_books +=1
-            record.total_available_books = tot_books
-            tot_books = 0
-
+            record.total_available_books = len(record.books)
+            
